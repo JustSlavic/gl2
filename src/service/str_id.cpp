c73092c202ca966b58f595625f9fbf5cbc5b36c8
radko <justslavic@gmail.com>
2021-02-07 17:23:30 +0300

    Add str_id class

--- src/service/str_id.cpp
+++ src/service/str_id.cpp
@@ -3,12 +3,66 @@
 
 #include <vector>
 #include <string>
+#include <cstring>
+
 
 namespace service {
 
-str_id::str_id (const char* id_) 
-    : id(id_)
-{}
+constexpr size_t POOL_SIZE = (1 << 10); // 1024 bytes (1 Kb)
+struct str_id_pool {
+    char* data = nullptr;
+    char* end = nullptr;
+    bool initialized = false;
+
+    ~str_id_pool() {
+        terminate();
+    }
+
+    void initialize () {
+        data = (char*)calloc(POOL_SIZE, sizeof(char));
+        end = data;
+        initialized = true;
+    }
+
+    void terminate () {
+        free(data);
+        data = nullptr;
+        end = nullptr;
+        initialized = false;
+    }
+
+    // @Todo: Efficiency
+    // @Todo: Thread-safe
+    const char* create (const char* id) {
+        char* word = data;
+
+        while (word != end) {
+            bool found = strcmp(word, id) == 0;
+            if (found) return word;
+            
+            word += strlen(word) + 1;
+        }
+
+        if ((u64(word) + strlen(id) + 1) > (u64(data) + POOL_SIZE - 1)) {
+            ASSERT(false);
+        }
+        strcpy(word, id);
+        end += strlen(word) + 1;
+        return word;
+    }
+};
+
+
+str_id_pool pool;
+
+
+str_id::str_id (const char* id_) {
+    if (!pool.initialized) {
+        pool.initialize();
+    }
+
+    id = pool.create(id_);
+}
 
 bool operator == (const str_id& lhs, const str_id& rhs) {
     return lhs.id == rhs.id;
--- tests/str_id.cpp
+++ tests/str_id.cpp
@@ -1,18 +1,23 @@
 #include <gtest/gtest.h>
+#include <defines.h>
 #include <service/str_id.hpp>
 
 
-TEST(StringID, Equal) {
+TEST(String_Id, Equal_Not_Equal) {
     service::str_id id1("SHADER_TRIANGLE");
     service::str_id id2("SHADER_TRIANGLE");
+    service::str_id id3("SHADER_ARROW");
 
     EXPECT_EQ(id1, id2);
+    EXPECT_NE(id1, id3);
 }
 
-TEST(StringID, NotEqual) {
-    service::str_id id1("SHADER_TRIANGLE");
-    service::str_id id2("SHADER_ARROW");
+TEST(String_Id, Overload) {
 
-    EXPECT_NE(id1, id2);
+#define CREATE_ID(n) service::str_id id##n("UI_SHADER_" STRINGIFY(n))
+    for (size_t i = 0; i < 78; i++) {
+        CREATE_ID(i);
+    }
+#undef CREATE_ID
 }
 
