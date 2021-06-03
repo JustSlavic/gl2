3254e8f46d9e615a5dd4d277d87cce288d84fd05
radko <justslavic@gmail.com>
2021-06-03 10:10:08 +0300

    Add test implementation of the new event system in main

--- main.cpp
+++ main.cpp
@@ -1,40 +1,185 @@
 #include <iostream>
-#include <utils.h>
-#include <version.h>
 #include <application.h>
+#include <version.h>
 #include <logging/logging.h>
 
-#include <stdarg.h>
+#include <deque>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 
-template <typename... Args>
-int log(const char* fmt, Args... args) {
-    return 0;
-}
+namespace EE {
+    enum class EventType {
+        mouse_event,
+        keyboard_event,
+    };
 
-int print2(const char* fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
+    struct IEvent {
+        bool handled = false;
 
-    while (*fmt) {
-        if (*fmt == '{' and *(fmt+1) == '}') {
-            int i = va_arg(args, int);
-            printf("%d", i);
-            fmt++;
-        } else {
-            printf("%c", *fmt);
+        virtual std::string get_name() = 0;
+        virtual EventType get_type() = 0;
+    };
+
+#define DECLARE_EVENT_TYPE(TYPE) \
+    static EventType get_static_type() { return TYPE; } \
+    EventType get_type() override { return get_static_type(); }
+
+    struct EventClick : public IEvent {
+        int x = 0;
+        int y = 0;
+
+        EventClick(int x, int y) : x(x), y(y) {}
+
+        std::string get_name() override { return "EventClick"; }
+        DECLARE_EVENT_TYPE(EventType::keyboard_event);
+    };
+
+    struct EventMouseMove : public IEvent {
+        std::string get_name() override { return "EventClick"; }
+        DECLARE_EVENT_TYPE(EventType::mouse_event);
+    };
+
+    struct EventQueue {
+        std::deque<IEvent*> q;
+
+        std::mutex m_mutex;
+        std::condition_variable cv;
+
+        void push_event(IEvent* e) {
+            std::lock_guard lock(m_mutex);
+
+            q.push_back(e);
+            cv.notify_one();
         }
 
-        fmt++;
-    }
+        IEvent* get_event() {
+            IEvent* result = nullptr;
 
-    va_end(args);
+            {
+                std::unique_lock lock(m_mutex);
+                cv.wait(lock, [this]() { return !this->q.empty(); });
 
-    return 0;
+                result = q.front();
+            }
+
+            return result;
+        }
+
+        void pop_event() {
+            if (q.empty()) { return; }
+
+            q.pop_front();
+        }
+    };
+
+    class Dispatcher {
+        template<typename EventType, class C, bool(C::* M)(EventType*)>
+        static bool invoke(void* instance, IEvent* e) {
+            if (EventType::get_static_type() != e->get_type()) return false;
+            return (static_cast<C*>(instance)->*M)(static_cast<EventType*>(e));
+        }
+
+    public:
+        template<typename EventType, class C, bool(C::* M)(EventType*)>
+        void bind(C* instance) {
+            auto pair = std::make_pair(&invoke<EventType, C, M>, instance);
+            targets.push_back(pair);
+        }
+
+        bool operator()(IEvent* e) {
+            for (auto pair : targets) {
+                if ((pair.first)(pair.second, e)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    private:
+        using Fn = bool(*)(void*, IEvent*);
+        std::vector<std::pair<Fn, void*>> targets;
+    };
+
+    struct EventReceiver {
+        Dispatcher d;
+
+        template <typename EventType, typename Host, bool(Host::*M)(EventType*)>
+        void bind(Host* host) {
+            d.bind<EventType, Host, M>(host);
+        }
+
+        bool handle(IEvent* e) {
+            return d(e);
+        }
+    };
+
+    struct UiLayer : public EventReceiver {
+        UiLayer() {
+            bind<EventClick, UiLayer, &UiLayer::on_click>(this);
+            bind<EventMouseMove, UiLayer, &UiLayer::on_mouse_move>(this);
+        }
+
+        bool on_click(EventClick* e) {
+            if (e->x > 0) {
+                printf("On click event handled! (%d, %d)\n", e->x, e->y);
+                return true;
+            }
+            return false;
+        }
+
+        bool on_mouse_move(EventMouseMove* e) {
+            printf("Move move event handled!\n");
+            return true;
+        }
+    };
+
+    struct WorldLayer : public EventReceiver {
+        WorldLayer() {
+            bind<EventClick, WorldLayer, &WorldLayer::on_click>(this);
+        }
+
+        bool on_click(EventClick* e) {
+            printf("Character shoots: pew pew!!!\n");
+            return true;
+        }
+    };
 }
 
 
 int main(int argc, char** argv, char** env) {
+    {
+        EE::EventQueue queue;
+        queue.push_event(new EE::EventClick(100, 100));
+        queue.push_event(new EE::EventMouseMove());
+        queue.push_event(new EE::EventClick(-10, 100));
+        queue.push_event(new EE::EventMouseMove());
+        queue.push_event(new EE::EventMouseMove());
+        queue.push_event(new EE::EventMouseMove());
+        queue.push_event(new EE::EventMouseMove());
+        queue.push_event(new EE::EventClick(20, 20));
+
+        std::vector<EE::EventReceiver*> layer_stack;
+        layer_stack.push_back(new EE::UiLayer());
+        layer_stack.push_back(new EE::WorldLayer());
+
+        EE::Dispatcher d;
+
+        while (!queue.q.empty()) {
+            auto pEvent = queue.get_event();
+            for (auto& layer : layer_stack) {
+                bool handled = layer->handle(pEvent);
+                if (handled) break;
+            }
+            queue.pop_event();
+        }
+    }
+
+    return 0;
+
+
     LogGlobalContext::instance()
         .set_level(Log::Level::Info)
         .attach(std::cout);
